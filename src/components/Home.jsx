5d819a45c48b393bfab2e624b3a740d8897ea71b
Bhavesh Mihani <bhaveshmihani07@gmail.com>
2025-09-09 22:19:07 +0530

    yes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ import { FaReact } from "react-icons/fa6";
 import { FaNodeJs } from "react-icons/fa6";
 
 import {ReactTyped} from "react-typed";
-import pic from '/undraw.svg'
+import pic from '/WhatsApp.jpg'
 
 const Home = () => {
   return (
@@ -85,7 +85,7 @@ const Home = () => {
           </div>
           <div className="md:w-1/2 flex justify-end items-center order-1 md:order-2">
             <div className="relative flex justify-center items-center group">
-              <img src="src/assets/WhatsApp Image 2024-04-12 at 22.26.45_567438dc.jpg" alt="" className="rounded-full w-[270px] h-[270px] md:w-[420px] md:h-[400px] object-cover transition-transform duration-500 ease-out group-hover:scale-105 m-5"/>
+              <img src={pic} alt="" className="rounded-full w-[270px] h-[270px] md:w-[420px] md:h-[400px] object-cover transition-transform duration-500 ease-out group-hover:scale-105 m-5"/>
             </div>
           </div>
         </div>
